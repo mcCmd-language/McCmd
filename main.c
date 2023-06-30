eb284ae2bbacf4f6ea224f739ecea1d5ea3d6673
minyee2913 <leegm0759@gmail.com>
2023-06-30 19:30:54 +0900

    string, bool 구현

--- main.c
+++ main.c
@@ -10,13 +10,13 @@ int* splice(int*, int, int, int);
 int main(int argc, char* argv[]) {
 
 	FILE* fp;
-	char* fileName = "./test/test.mcCmd";
+	char* fileName = "../test/test.mcCmd";
 	printf("%s", fileName);
 
 	fp = fopen(fileName, "r");
 
 	if (fp == NULL) {
-		printf_s("파일을 찾을 수 없습니다.");
+		printf_s("\n파일을 찾을 수 없습니다.");
 
 		return 0;
 	}
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
 	token = malloc(sizeof(int) * size);
 
 	if (token == NULL) {
-		printf("token is NULL");
+		printf("\ntoken is NULL");
 		return 0;
 	}
 
@@ -52,10 +52,11 @@ long fsize(FILE* fp) {
 
 
 //define = 0, defineVariable = 1, variableArgs = 2, defineFunc = 3, funcVariable = 4, callFunc = 5 
-//variableType.. null ＝ -1 int = 0, float = 1, double = 2, string = 3, array = 4, json = 5
+//variableType.. null ＝ -1 int = 0, float = 1, double = 2, bool = 3, string = 4, array = 5, json = 6
 struct Variable {
 	char* name;
 	int type;
+	int valueBool;
 	int valueInt;
 	char* valueStr;
 	char valueChar;
@@ -73,9 +74,12 @@ void parseCode(int* token, int size) {
 
 	int varIndex = 0;
 	int varDeclared = 0;
+	int parsingString = 0;
 	struct Variable* variables;
 	variables = (struct Variable*)malloc(sizeof(struct Variable) * size);
 	if (variables == NULL) return;
+
+	int funcCalled = 0;
 
 	for (int i = 0; i < size; i++) {
 		char t = token[i];
@@ -83,6 +87,10 @@ void parseCode(int* token, int size) {
 			if (t == '@') {
 				parseStart = i;
 				parseType = 0;
+			}
+			else if (t == '/') {
+				parseStart = i;
+				parseType = 5;
 			}
 		}
 		else {
@@ -99,6 +107,7 @@ void parseCode(int* token, int size) {
 					if (t == ';') {
 						parseType = -1;
 						varDeclared = 0;
+						parsingString = 0;
 					}
 					parseEnd = i - 1;
 
@@ -122,7 +131,11 @@ void parseCode(int* token, int size) {
 					if (t == '[') {
 						parseStart = i;
 					}
-					else if (t == ',' || t == ']') {
+					else if (t == '"') {
+						if (!parsingString) parsingString = 1;
+						else parsingString = 0;
+					}
+					else if ((t == ',' || t == ']') && !parsingString) {
 						if (t == ']') parseType = -1;
 						parseEnd = i - 1;
 
@@ -131,6 +144,7 @@ void parseCode(int* token, int size) {
 						int a_index = 0;
 
 						int valIndex;
+						int vvIndex;
 						int isValue = 0;
 						for (int j = 0; j <= parseEnd - parseStart; j++) {
 							int to = token[parseStart + j + 1];
@@ -138,11 +152,17 @@ void parseCode(int* token, int size) {
 							if (to == '=') {
 								valIndex = j;
 								isValue = 1;
+								vvIndex = j - valIndex;
 								continue;
 							}
 
 							if (isValue) {
-								varValue[j - valIndex - 1] = to;
+								if (to == '\\') continue;
+								if (token[parseStart + j] != '\\' && to == '"') {
+									continue;
+								}
+								varValue[vvIndex] = to;
+								vvIndex++;
 							}
 							else {
 								if (to == ' ' || to == '\n') continue;
@@ -153,7 +173,7 @@ void parseCode(int* token, int size) {
 						}
 
 						varArg[a_index] = '\0';
-						varValue[parseEnd - parseStart - valIndex - 1] = '\0';
+						varValue[vvIndex - 1] = '\0';
 
 						printf("\n%s - varArg", varArg);
 						printf("\n%s - varValue", varValue);
@@ -162,10 +182,18 @@ void parseCode(int* token, int size) {
 						if (strcmp(varArg, "type") == 0) {
 							if (strcmp(varValue, "int") == 0) {
 								variables[varIndex - 1].type = 0;
-							} else if (strcmp(varValue, "float") == 0) {
+							}
+							else if (strcmp(varValue, "float") == 0) {
 								variables[varIndex - 1].type = 1;
-							} else if (strcmp(varValue, "double") == 0) {
+							}
+							else if (strcmp(varValue, "double") == 0) {
 								variables[varIndex - 1].type = 2;
+							}
+							else if (strcmp(varValue, "bool") == 0) {
+								variables[varIndex - 1].type = 3;
+							}
+							else if (strcmp(varValue, "string") == 0) {
+								variables[varIndex - 1].type = 4;
 							}
 						}
 						else if (strcmp(varArg, "value") == 0) {
@@ -179,12 +207,78 @@ void parseCode(int* token, int size) {
 								case 2:
 									variables[varIndex - 1].valueDouble = (double)atof(varValue);
 									break;
+								case 3:
+									if (strcmp(varValue, "true") == 0) variables[varIndex - 1].valueBool = 1;
+									else if (strcmp(varValue, "false") == 0) variables[varIndex - 1].valueBool = 0;
+
+									break;
+								case 4:
+									variables[varIndex - 1].valueStr = varValue;
+									break;
 								default:
 									break;
 							}
 						}
 
 						if (t == ',') parseStart = i;
+					}
+				}
+			}
+			else if (parseType == 5) {
+				printf("\nparse2");
+				if (t == ';') {
+					parseType = -1;
+
+					char** args = (char**)malloc(sizeof(char*)* parseEnd - parseStart);
+					int* argsSizes = (int*)malloc(sizeof(int)* parseEnd - parseStart);
+
+					int argStart = 0;
+					int argEnd;
+					char to;
+
+					int argIndex = 0;
+					for (int j = 0; j <= parseEnd - parseStart + 1; j++) {
+						to = token[parseStart + j + 1];
+
+						if (to == ' ') {
+							argEnd = j - 1;
+
+							argsSizes[argIndex] = argEnd - argStart;
+							for (int s = 0; s <= argEnd - argStart; s++) {
+								args[argIndex][s] = token[parseStart + argStart + 1];
+							}
+
+							args[argIndex][parseStart + argEnd + 1] = '\0';
+							printf("%s", args[argIndex]);
+							argIndex++;
+						}
+					}
+					printf("\n%s", args);
+
+					if (strcmp(args[0], "say") == 0) {
+						parsingString = 0;
+
+						char* string = (char*)malloc(sizeof(char)* argsSizes[1]);
+						int strIndex = 0;
+
+						for (int j = 0; j <= argsSizes[1]; j++) {
+							to = args[1][j];
+
+							if (to == '"') {
+								if (parsingString) {
+									parsingString = 0;
+								}
+								else {
+									parsingString = 1;
+								}
+							}
+							else if (parsingString) {
+								string[strIndex] = to;
+								strIndex++;
+							}
+						}
+
+						puts(string);
 					}
 				}
 			}
@@ -197,6 +291,11 @@ void parseCode(int* token, int size) {
 		if (variables[i].type == 0) printf("\n%s(int) - %d", variables[i].name, variables[i].valueInt);
 		else if (variables[i].type == 1) printf("\n%s(float) - %.3lf", variables[i].name, variables[i].valueFloat);
 		else if (variables[i].type == 2) printf("\n%s(double) - %.3lf", variables[i].name, variables[i].valueDouble);
+		else if (variables[i].type == 3) {
+			if (variables[i].valueBool == 1) printf("\n%s(bool) - true", variables[i].name);
+			else printf("\n%s(bool) - false", variables[i].name);
+		}
+		//else if (variables[i].type == 4) printf("\n%s(string) - %s", variables[i].name, variables[i].valueStr);
 	}
 }
 
